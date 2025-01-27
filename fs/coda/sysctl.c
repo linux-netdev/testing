5f80bef46bb98685111c51eb92d643e7a72065e8
Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>
2025-01-27 15:20:03 +0100

    sysctl/coda: Fixes timeout bounds
    Bound coda timeout sysctl writings between SYSCTL_ZERO
    and SYSCTL_INT_MAX.
    
    The proc_handler has thus been updated to proc_dointvec_minmax.
    
    Signed-off-by: Nicolas Bouchinet <nicolas.bouchinet@ssi.gouv.fr>
    Signed-off-by: NipaLocal <nipa@local>

--- fs/coda/sysctl.c
+++ fs/coda/sysctl.c
@@ -20,7 +20,9 @@ static struct ctl_table coda_table[] = {
 		.data		= &coda_timeout,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_INT_MAX,
 	},
 	{
 		.procname	= "hard",
