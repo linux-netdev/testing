e70af9815c541d1c5c179fbf7bf17dff679e3832
Kory Maincent <kory.maincent@bootlin.com>
2025-01-28 16:35:47 +0100

    net: ethtool: tsconfig: Fix ts filters and types enums size check
    Fix the size check for the hwtstamp_tx_types and hwtstamp_rx_filters
    enumerations. Align this check with the approach used in tsinfo for
    consistency and correctness.
    
    Signed-off-by: Kory Maincent <kory.maincent@bootlin.com>
    Fixes: 6e9e2eed4f39 ("net: ethtool: Add support for tsconfig command to get/set hwtstamp config")
    Signed-off-by: NipaLocal <nipa@local>

--- net/ethtool/tsconfig.c
+++ net/ethtool/tsconfig.c
@@ -294,8 +294,8 @@ static int ethnl_set_tsconfig(struct ethnl_req_info *req_base,
 	struct nlattr **tb = info->attrs;
 	int ret;
 
-	BUILD_BUG_ON(__HWTSTAMP_TX_CNT >= 32);
-	BUILD_BUG_ON(__HWTSTAMP_FILTER_CNT >= 32);
+	BUILD_BUG_ON(__HWTSTAMP_TX_CNT > 32);
+	BUILD_BUG_ON(__HWTSTAMP_FILTER_CNT > 32);
 
 	if (!netif_device_present(dev))
 		return -ENODEV;
